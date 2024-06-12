77df404501357bc2b9fd7f69255ecd04194a5ad5
Vyshnavi P <116145827+vyshnavi9241@users.noreply.github.com>
2024-06-12 20:43:47 +0900

    Time: 1 ms (98.99%), Space: 45.3 MB (43.32%) - LeetHub

---
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -0,0 +1,53 @@
+class Solution {
+    public void solve(char[][] board) {
+        if (board == null || board.length == 0) return;
+        
+        int m = board.length;
+        int n = board[0].length;
+        
+        // Traverse the first and last columns
+        for (int i = 0; i < m; i++) {
+            if (board[i][0] == 'O') {
+                dfs(board, i, 0);
+            }
+            if (board[i][n - 1] == 'O') {
+                dfs(board, i, n - 1);
+            }
+        }
+        
+        // Traverse the first and last rows
+        for (int j = 0; j < n; j++) {
+            if (board[0][j] == 'O') {
+                dfs(board, 0, j);
+            }
+            if (board[m - 1][j] == 'O') {
+                dfs(board, m - 1, j);
+            }
+        }
+        
+        // Flip captured regions
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 'O') {
+                    board[i][j] = 'X';
+                } else if (board[i][j] == '#') {
+                    board[i][j] = 'O';
+                }
+            }
+        }
+    }
+    
+    private void dfs(char[][] board, int row, int col) {
+        if (row < 0 || col < 0 || row >= board.length || col >= board[0].length || board[row][col] != 'O') {
+            return;
+        }
+        
+        board[row][col] = '#'; // Mark as visited
+        
+        // Explore adjacent cells
+        dfs(board, row - 1, col);
+        dfs(board, row + 1, col);
+        dfs(board, row, col - 1);
+        dfs(board, row, col + 1);
+    }
+}
