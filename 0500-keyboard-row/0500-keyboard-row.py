e8220894d0fc94cf359e62fdd6c9ca3aa070c72b
Vyshnavi P <116145827+vyshnavi9241@users.noreply.github.com>
2024-11-04 10:17:50 +0530

    Time: 0 ms (100%), Space: 16.6 MB (42.59%) - LeetHub

--- 0500-keyboard-row/0500-keyboard-row.py
+++ 0500-keyboard-row/0500-keyboard-row.py
@@ -2,20 +2,12 @@ from typing import List
 
 class Solution:
     def findWords(self, words: List[str]) -> List[str]:
-        # Define the sets for each row on the keyboard
         row1 = set("qwertyuiop")
         row2 = set("asdfghjkl")
         row3 = set("zxcvbnm")
-        
-        # List to store words that can be typed using one row
         result = []
-        
-        # Check each word
         for word in words:
-            # Convert the word to lowercase to handle both uppercase and lowercase
             lower_word = set(word.lower())
-            
-            # Check if all letters in the word belong to one of the rows
             if lower_word.issubset(row1) or lower_word.issubset(row2) or lower_word.issubset(row3):
                 result.append(word)
         
